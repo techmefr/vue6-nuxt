a765040a304a46893f33718ff590cbc055c2a183
techmefr <gaetan-compigni@wilder.school>
2025-03-25 16:38:31 +0100

    fix folder's name layouts

---
+++ layouts/custom.vue
@@ -0,0 +1,5 @@
+<template>
+    <header>Pomme</header>
+    <slot />
+    <footer>Cannelle</footer>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
     <h1>Page d'accueil</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi temporibus maiores voluptatum, cumque ducimus ab
         dignissimos, iste debitis est nam dolores? Omnis vitae ipsa sequi recusandae dolores maxime ipsam impedit.</p>
-    <NuxtLink to="/blog">Page de blog</NuxtLink>
+    <NuxtLink to="/blog">Page de blog</NuxtLink><br>
+    <NuxtLink to="/contact">Page contact</NuxtLink>
+
 </template>
 
 <script></script>
