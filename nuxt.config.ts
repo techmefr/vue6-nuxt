cdbf4b8b9d4ce4f6dd9b890376d689e9ad1510f0
techmefr <gaetan-compigni@wilder.school>
2025-03-26 15:24:03 +0100

    comment to know how use cdn for animate.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  // pour utiliser le cdn à la place d'installer la librarie 
+  /*
+  app: {
+  head: {
+   link: [{rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css
+        }] 
+    }
+  }
+  */
   css: ['~/assets/test.css', 'animate.css'],
   devServer: {
     port: 52133,  // mettre le numéro de port qu'utilise StackBlitz
