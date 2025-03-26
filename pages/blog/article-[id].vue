31d3d917da6fefa48367e9c9a6e3d6fc9866d339
techmefr <gaetan-compigni@wilder.school>
2025-03-26 15:38:59 +0100

    init structure about [id]

---
+++ pages/blog/article-[id].vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Article 1</h1>
+    <p>Contenue de l'article 1 :
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste corrupti, natus molestias quas aliquid
+        mollitia debitis deserunt quidem similique omnis vel aut quo explicabo, ducimus praesentium amet illo non.</p>
+<NuxtLink to="/blog">Retour au blog</NuxtLink>
+</template>
--- pages/blog/article1.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h1>Article 1</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste corrupti, natus molestias quas aliquid
-        mollitia debitis deserunt quidem similique omnis vel aut quo explicabo, ducimus praesentium amet illo non.</p>
-</template>
