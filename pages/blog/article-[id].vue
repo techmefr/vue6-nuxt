7bb629b959666ed4661a659e6bc11fe1f8015489
techmefr <gaetan-compigni@wilder.school>
2025-03-26 15:48:50 +0100

    add dynamic page

--- pages/blog/article-[id].vue
+++ pages/blog/article-[id].vue
@@ -1,7 +1,14 @@
 <template>
-    <h1>Article 1</h1>
-    <p>Contenue de l'article 1 :
+    <h1>Article {{ route.params.id }}</h1>
+    <p>Contenue de l'article {{ route.params.id }} :
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum iste corrupti, natus molestias quas aliquid
         mollitia debitis deserunt quidem similique omnis vel aut quo explicabo, ducimus praesentium amet illo non.</p>
-<NuxtLink to="/blog">Retour au blog</NuxtLink>
+    <NuxtLink to="/blog">Retour au blog</NuxtLink>
 </template>
+
+<script>
+const route = useRoute()
+const id = route.params.id
+
+//fetch API pour récupérer le contenu par son id
+</script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,7 +1,16 @@
 <template>
-    <h1>Page d'accueil</h1>
-    <p>Liste des articles</p>
-    <NuxtLink to="/blog/article1">Lire l'article 1</NuxtLink>
+    <h1>Liste des articles</h1>
+    <ul>
+        <li v-for="o in 5">
+            <NuxtLink :to="'/blog/article-' + i">Article {{ i }}</NuxtLink>
+        </li>
+
+    </ul>
+    <NuxtLink to="/blog/article-1">Lire l'article 1</NuxtLink>
+
+    {{ route }}
+
 </template>
 
-<script></script>
+<script>
+</script>
